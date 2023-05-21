0a8e8a75192cf248dfb35fdf9230043fef873a84
Faruk Karaman <134199695+FarukKaramanVektorel@users.noreply.github.com>
2023-05-21 20:07:15 +0300

    Create SayisalLoto.java

---
+++ SayisalLoto.java
@@ -0,0 +1,46 @@
+/*
+ by Faruk Karaman
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SayisalLoto {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Random rnd=new Random();
+        boolean oyun = true;
+        boolean reNumber = false;
+        int[] loto = new int[6];
+        while (oyun) {
+            for (int i = 0; i < 6; i++) {
+                reNumber = false;
+                while (reNumber == false) {
+                    int k = 0;
+                    int randomNumber=rnd.nextInt(49)+1;
+                    for (k = 0; k < 6; k++) {
+                        if (loto[k] == randomNumber) {
+                            break;
+                        }
+                    }
+                    if (k == 6) {
+                        loto[i] = randomNumber;
+                        reNumber = true;
+                    }
+                }
+            }
+            Arrays.sort(loto);
+            System.out.println("Tahmini sayisal loto sonuclari");
+            System.out.println(Arrays.toString(loto));
+            System.out.println("Tekrar oynamak istermisiniz (e/h)?");
+            String evetHayir = scn.next();
+            if (evetHayir.equals("e")) {
+                oyun = true;
+            } else if (evetHayir.equals("h")) {
+                oyun = false;
+                System.out.println("Oyun sonlandırıldı.");
+            }
+        }
+    }
+}
